e064ca19133868c597d46610f5344b039c7a656b
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-1129.local>
2021-09-21 14:52:39 +0100

    passing arguments into methods

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,21 @@ import { useState } from "react"
 const App = () => {
     const [persons, setPersons] = useState([{name: "leon"},{name: "jordan"}])
     
-    const bananas = () => {
-        console.log("clicky click click")
+    const handleClick = (name) => {
+        console.log("Hello " + name)
     }
 
     return (
         <div>
             <h1>CN Staff members</h1>
-            <Person name={persons[0].name} click={bananas} />
-            <Person name={persons[1].name} click={bananas} />
+            <Person name={persons[0].name} click={handleClick} />
+            <Person name={persons[1].name} click={handleClick} />
         </div>
     )
 }
 
 const Person = (props) => {
-    return <h4 onClick={props.click}>{props.name}</h4>
+    return <h4 onClick={() => props.click(props.name)}>{props.name}</h4>
 }
 
 export default App
